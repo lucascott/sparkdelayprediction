c89d681894489100c8ec27682ec573d360b64995
Luca Scotton <lucascott96@hotmail.it>
2019-01-27 14:35:53 +0100

    constants refactor

--- src/main/scala/sparkproject/Constants.scala
+++ src/main/scala/sparkproject/Constants.scala
@@ -5,10 +5,10 @@ object Constants {
   val flightId = "FlightNum"
   val prohibitedVariables: Seq[String] = Seq("ArrTime", "ActualElapsedTime", "AirTime", "TaxiIn", "Diverted",
     "CarrierDelay", "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay")
-  val moreDroppedVariables: Seq[String] = Seq("CRSDepTime", "CRSArrTime", "TailNum", "CancellationCode")
+  val moreDroppedVariables: Seq[String] = Seq("CRSDepTime", "CRSArrTime", "TailNum", "CancellationCode", "CRSElapsedTime", "Distance")
   val oneHotEncVariables: Seq[String] = Seq("UniqueCarrier", "Origin", "Dest")
   val featureVariables: Seq[String] = Seq("Year", "Month", "DayofMonth", "DayOfWeek", "DepTime",
-    "DepDelay", "TaxiOut", "UniqueCarrierEnc", "OriginEnc") //"CRSElapsedTime", "DestEnc", "Distance")
+    "DepDelay", "TaxiOut", "UniqueCarrierEnc", "OriginEnc") //, "DestEnc")
   val labelVariable: String = "ArrDelay"
   val predictionCol = "prediction"
   val metric: String = "rmse"
